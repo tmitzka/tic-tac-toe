e43f65d4a72636c26da98f2bfd117c6098b3c6df
Thomas Mitzka <tm@thomasmitzka.at>
2023-06-24 07:51:05 +0200

    Change main function to play a complete game

--- main.py
+++ main.py
@@ -14,10 +14,20 @@ def main():
     ])
     # print(game)
     while True:
-        game.show_grid()
-        game.place_mark()
+        current_player = game.players[0]
+        indices_empty = game.get_indices_empty()
+        if current_player.is_human:
+            game.show_grid()
+            chosen_index = current_player.choose_field_human(indices_empty)
+        else:
+            index_win_block = game.win_or_block()
+            if index_win_block:
+                chosen_index = index_win_block
+            else:
+                chosen_index = current_player.choose_field_computer(indices_empty)
+        game.place_mark(chosen_index)
         if game.three_in_a_row():
-            print(f"\n{game.players[0].name} won!")
+            print(f"\n{current_player.name} won!")
             game.show_grid()
             break
         game.switch_players()
