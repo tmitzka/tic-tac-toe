cacbcf9d3a6b149b63eb0fdb216ca8fc79d1d5f7
Thomas Mitzka <tm@thomasmitzka.at>
2023-06-24 09:07:03 +0200

    Add the option to declare a draw

--- main.py
+++ main.py
@@ -18,12 +18,15 @@ def main():
     while True:
         player = game.players[0]
         indices_empty = game.get_indices_empty()
+        final_turn = True if len(indices_empty) == 1 else False
 
         if player.is_human:
+            # Let the human player choose a field.
             game.show_grid()
             sleep(2)
             chosen_index = player.choose_field_human(indices_empty)
         else:
+            # Let the computer player choose a field.
             index_win_block = game.win_or_block()
             if index_win_block:
                 chosen_index = index_win_block
@@ -33,12 +36,18 @@ def main():
         game.place_mark(chosen_index)
         sleep(2)
 
-        if game.three_in_a_row():
-            if player.is_human:
-                print("\nYou won!")
+        # If all fields are taken, declare a winner or a draw.
+        if final_turn:
+            if game.three_in_a_row():
+                if player.is_human:
+                    print(f"\n{player.name}, you won!")
+                else:
+                    print(f"\n{player.name} won!")
             else:
-                print(f"\n{player.name} won!")
+                print("\nIt's a draw!")
             game.show_grid()
+
+            # Let the user decide whether to start a new game.
             if game.play_again():
                 game.start_new_game(players)
             else:
